c7851e23546991680dcf5a53f41b67dd66d240c7
Marcello Rego <marcellorego@gmail.com>
2016-12-29 22:30:08 +1300

    Loading composition

--- lesson4/src/main/java/br/com/splessons/dao/AbstractHibernateDao.java
+++ lesson4/src/main/java/br/com/splessons/dao/AbstractHibernateDao.java
@@ -2,46 +2,71 @@ package br.com.splessons.dao;
 
 import java.lang.reflect.ParameterizedType;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
- 
+
 public abstract class AbstractHibernateDao<T> {
- 
+
 	protected Class<T> persistentClass;
-	
-    @Autowired
-    private SessionFactory sessionFactory;
- 
-    @SuppressWarnings("unchecked")
-    public AbstractHibernateDao() {
-    	super();
-    	
-    	this.persistentClass = (Class<T>)
-		   ((ParameterizedType)getClass().getGenericSuperclass())
-		      .getActualTypeArguments()[0];
-    }
-    
-    protected Session getSession() {
-        return sessionFactory.getCurrentSession();
-    }
-    
-    public void persist(Object entity) {
-        getSession().persist(entity);
-    }
- 
-    public void delete(Object entity) {
-        getSession().delete(entity);
-    }
-    
-    @SuppressWarnings("unchecked")
-    public T get(Long id) {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@SuppressWarnings("unchecked")
+	public AbstractHibernateDao() {
+		super();
+
+		this.persistentClass = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass())
+				.getActualTypeArguments()[0];
+	}
+
+	protected Session getSession() {
+		return sessionFactory.getCurrentSession();
+	}
+
+	public void persist(Object entity) {
+		getSession().persist(entity);
+	}
+
+	public void delete(Object entity) {
+		getSession().delete(entity);
+	}
+
+	@SuppressWarnings("unchecked")
+	public T get(Long id) {
 		Session session = getSession();
-        T result = (T) session.get(persistentClass, id);
-        return result;
-    }
-    
-    public void deleteAll() {
-    	getSession().createQuery("delete from " + persistentClass.getSimpleName().toUpperCase()).executeUpdate();
-    }
+		T result = (T) session.get(persistentClass, id);
+		return result;
+	}
+
+	public void deleteAll() {
+		getSession().createQuery("delete from " + persistentClass.getSimpleName().toUpperCase()).executeUpdate();
+	}
+
+	protected Criteria getCriteria(Class<T> clazz, String alias) {
+		Criteria criteria = null;
+		if (alias != null) {
+			criteria = this.sessionCreateCriteria(clazz, alias);
+		} else {
+			criteria = this.sessionCreateCriteria(clazz);
+		}
+		return criteria;
+	}
+
+	protected Criteria sessionCreateCriteria(Class<T> clazz) {
+		Session session = this.getSession();
+		return session.createCriteria(clazz);
+	}
+
+	protected Criteria sessionCreateCriteria(Class<T> classeNegocio, String alias) {
+		Session session = this.getSession();
+		return session.createCriteria(classeNegocio, alias);
+	}
+
+	protected Criteria getDefaultCriteria(String alias) {
+		Criteria criteria = this.getCriteria(this.persistentClass, alias);
+		return criteria;
+	}
 }
--- lesson4/src/main/java/br/com/splessons/dao/SalaryDaoImpl.java
+++ lesson4/src/main/java/br/com/splessons/dao/SalaryDaoImpl.java
@@ -3,9 +3,14 @@ package br.com.splessons.dao;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.FetchMode;
 import org.hibernate.Query;
 import org.hibernate.criterion.Order;
+import org.hibernate.criterion.ProjectionList;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.sql.JoinType;
+import org.hibernate.transform.AliasToBeanResultTransformer;
 import org.springframework.stereotype.Repository;
 
 import br.com.splessons.model.Employee;
@@ -22,10 +27,23 @@ public class SalaryDaoImpl extends AbstractHibernateDao<Salary> implements ISala
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<Salary> findByEmployee(Employee employee) {
-		Criteria criteria = getSession().createCriteria(Salary.class);
+		Criteria criteria = getDefaultCriteria("_salary");
+		//criteria.createAlias("_salary.employee", "_employee", JoinType.INNER_JOIN );
+		//criteria.setFetchMode("employee", FetchMode.JOIN);
+		
+		/*ProjectionList proList = Projections.projectionList();
+        proList.add(Projections.property("employee"), "employee");
+        proList.add(Projections.property("fromDate"), "fromDate");
+        proList.add(Projections.property("toDate"), "toDate");
+        proList.add(Projections.property("salary"), "salary");
+        criteria.setProjection(proList);*/
+        
         criteria.add(Restrictions.eq("employee", employee));
         criteria.addOrder(Order.asc("fromDate"));
-        return (List<Salary>) criteria.list();
+        //criteria.setResultTransformer(new AliasToBeanResultTransformer(Salary.class));
+        
+        List<Salary> result = (List<Salary>) criteria.list();
+        return result ;
 	}
 
 	@Override
