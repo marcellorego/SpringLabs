00454353299aebc3a932f1bf7a8cf8430f686e43
Marcello Rego <marcellorego@gmail.com>
2018-08-23 16:14:01 +0100

    Add simple service

---
+++ lesson13/simple-service/src/main/java/br/com/splessons/simpleservice/controller/SimpleController.java
@@ -0,0 +1,19 @@
+package br.com.splessons.simpleservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @Value("${user.environment}")
+    private String userEnv;
+
+    @GetMapping(value = "/env/{username}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String userEnv(@PathVariable("username") String username) {
+        return String.format("Hello %s! You are at %s environment\n", username, userEnv);
+    }
+}
